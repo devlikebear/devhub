ffef532388ca3bddfac646621549242eddb931a7
devlikebear <devlikebear@gmail.com>
2025-10-02 22:10:30 +0900

    fix: Navbar 메뉴를 유틸리티 플랫폼에 맞게 수정
    /projects, /blog 링크 제거하고 /tools 링크로 대체

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -9,8 +9,7 @@ export default function Navbar() {
   const navLinks = [
     { href: '/', label: 'Home' },
     { href: '/about', label: 'About' },
-    { href: '/projects', label: 'Projects' },
-    { href: '/blog', label: 'Blog' },
+    { href: '/tools', label: 'Tools' },
     { href: '/contact', label: 'Contact' },
   ];
 
